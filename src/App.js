e41c8ec3d4039297c3c7d2373a8563ac6cdb1ccd
Tanya Yang <tanyang@calvisitor-10-105-173-43.calvisitor.1918.berkeley.edu>
2018-11-03 16:40:32 -0700

    unsure

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
 import React, { Component } from 'react';
-import Login from './components/Login'
+import Login from './containers/Login'
 import './App.css';
 
 class App extends Component {
-  constructor (props){
-    super(props);
-    this.state = {
-
-    }
-  }
   render() {
     return (
-      <div className='app'>   
+      <div className='app'>
         <header>
             <div className='wrapper'>
               <h1>Pset Party</h1>
             </div>
         </header>
         <div className='container'>
-<<<<<<< HEAD
-          <section className='add-item'>
-              <form>
-                <input type="text" name="username" placeholder="What's your name?" />
-                <input type="text" name="currentItem" placeholder="What course are you?" />
-                <input type="text" name="email" placeholder="What's your Kerberos?" />
-                <input type="text" name="phonenumb" placeholder="What's your phone number?" />
-                <button>Submit</button>
-              </form>
-          </section>
-          <section className='display-item'>
-            <div className='wrapper'>
-              <ul>
-              </ul>
-            </div>
-          </section>
-=======
           <Login />
->>>>>>> 1bbf13aac397fd6bce9a3a49846bd8caca471f53
         </div>
       </div>
     );
