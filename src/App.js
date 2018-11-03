eb10a1f4fdc46507405626c2c805315defe1c757
SanjnaR <sanjna.ravichandar@gmail.com>
2018-11-03 13:35:57 -0400

    changed header to pset party

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className='app'>
         <header>
             <div className='wrapper'>
-              <h1>Fun Food Friends</h1>
+              <h1>Pset Party</h1>
               
             </div>
         </header>
