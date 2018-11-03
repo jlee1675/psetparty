7e770bc7071972cbd685606eba9f3d0073f74fca
Jessica Lee <jessicalee@calvisitor-10-105-187-61.calvisitor.1918.berkeley.edu>
2018-11-03 16:39:39 -0700

    added database util folder

--- src/App.js
+++ src/App.js
@@ -18,25 +18,7 @@ class App extends Component {
             </div>
         </header>
         <div className='container'>
-<<<<<<< HEAD
-          <section className='add-item'>
-              <form>
-                <input type="text" name="username" placeholder="What's your name?" />
-                <input type="text" name="currentItem" placeholder="What course are you?" />
-                <input type="text" name="email" placeholder="What's your Kerberos?" />
-                <input type="text" name="phonenumb" placeholder="What's your phone number?" />
-                <button>Submit</button>
-              </form>
-          </section>
-          <section className='display-item'>
-            <div className='wrapper'>
-              <ul>
-              </ul>
-            </div>
-          </section>
-=======
           <Login />
->>>>>>> 1bbf13aac397fd6bce9a3a49846bd8caca471f53
         </div>
       </div>
     );
