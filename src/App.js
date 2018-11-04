5f435ed83a86233a4ded21cc9aa110e50eb690dd
Jessica Lee <jessicalee@calvisitor-10-105-185-10.calvisitor.1918.berkeley.edu>
2018-11-04 01:14:14 -0800

    firebase works

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Login from './components/Login'
 import Profile from './components/Profile'
 import JoinForm from './components/JoinForm'
-import firestore from './firebase'
+import DataCollection from './util/DataCollection'
 import './App.css';
 
 
@@ -10,9 +10,41 @@ class App extends Component {
   constructor (props){
     super(props);
     this.state = {
-        users: [],
+        users: []
     }
+    this.loadData();
   }
+  findGroup(student) {
+    for (let other in this.state.users) {
+      if (other.subject == student.subject) {
+          if (other.time == student.time) {
+              this.setState({
+                users: this.state.users[student].push(other.id)
+              });   
+          }
+      }
+  }
+  }
+  async loadData() {
+    let data = await DataCollection.list(); 
+    this.setState({
+      users: data
+    });  
+   }
+  // loadData() {
+  //   const docRef = db.doc("users/1");
+  //   console.log("plsease work")
+  //   docRef.set({
+  //     int: 3
+  //   }).then(function(){
+  //     console.log("Status saved!");
+  //   }).catch(function(error){
+  //     console.log("Got an error: ", error)
+  //   });
+
+
+  // }
+  
   render() {
     return (
       <div className='app'>
@@ -21,6 +53,13 @@ class App extends Component {
               <h1>Pset Party</h1>
             </div>
         </header>
+        {this.state.users.map((doc, i) => {
+          return (
+            <div key={i}> 
+              <span> {doc.id} </span>
+            </div> 
+          )
+        })}
         <Login />
         
       </div>
